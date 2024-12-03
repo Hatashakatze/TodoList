e5734a9040865a21b21a8a73825145b8777c0e84
Natalja <wisterlover@gmail.com>
2024-12-03 13:21:52 +0500

    исправлено в соответствии с MVVM

--- TodoList/MainPage.xaml.cs
+++ TodoList/MainPage.xaml.cs
@@ -1,38 +1,64 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 
-namespace TodoList
+namespace TodoList;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainViewModel ViewModel { get; set; }
+
+    public MainPage()
     {
-        public ObservableCollection<TaskItem> Tasks { get; set; } = new ObservableCollection<TaskItem>();
-
-        public MainPage()
-        {
-            InitializeComponent();
-            TasksList.ItemsSource = Tasks;
-        }
-
-        private void OnAddTaskClicked(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrWhiteSpace(TaskEntry.Text))
-            {
-                Tasks.Add(new TaskItem { Name = TaskEntry.Text });
-                TaskEntry.Text = string.Empty;
-            }
-        }
-
-        private void OnDeleteTaskClicked(object sender, EventArgs e)
-        {
-            if (sender is Button button && button.BindingContext is TaskItem task)
-            {
-                Tasks.Remove(task);
-            }
-        }
+        InitializeComponent();
+        ViewModel = new MainViewModel();
+        BindingContext = ViewModel;
     }
 
     public class TaskItem
     {
         public string Name { get; set; }
         public bool IsCompleted { get; set; }
+    }
+
+    public class MainViewModel : BindableObject
+    {
+        private string _newTaskName;
+        public string NewTaskName
+        {
+            get => _newTaskName;
+            set
+            {
+                _newTaskName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<TaskItem> Tasks { get; set; } = new ObservableCollection<TaskItem>();
+
+        public ICommand AddTaskCommand { get; }
+        public ICommand DeleteTaskCommand { get; }
+
+        public MainViewModel()
+        {
+            AddTaskCommand = new Command(AddTask);
+            DeleteTaskCommand = new Command<TaskItem>(DeleteTask);
+        }
+
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskName))
+            {
+                Tasks.Add(new TaskItem { Name = NewTaskName });
+                NewTaskName = string.Empty;
+            }
+        }
+
+        private void DeleteTask(TaskItem task)
+        {
+            if (Tasks.Contains(task))
+            {
+                Tasks.Remove(task);
+            }
+        }
     }
 }
